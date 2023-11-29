1a68a22726a1e5b7b4d54cfac5a2a1db82647944
Niprois <alvarof628@gmail.com>
2023-11-29 10:02:58 -0300

    case 1 & 2 done

--- Proyecto2.cpp
+++ Proyecto2.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <string.h>
+#include <windows.h>
 
 using namespace std;
 
 struct Guardian{   //Definición de guardian y sus caracteristicas, como nombre, NivelDePoder, ciudad, maestro y aprendices.
-string Name;
-int PowerLevel;
-string City;
-string Master;
-string Apprentice[8];
+    char Name[20];
+    int PowerLevel;
+    char Master[20];
+    char City[20];
+    string Apprentice[20];
 };
 
-struct Node {
+struct Node { 
     int data;
     Node* left;
     Node* right;
@@ -65,19 +66,57 @@ int main()
         return 0;
     }
     
+    struct Guardian nguardian[50];
+
+    int guardcount = 0;
+    //Se ingresan los guardianes dentro del struct
+    while (fscanf(file, "%[^,],%d,%[^,],%[^\n]" , nguardian[guardcount].Name ,&nguardian[guardcount].PowerLevel,
+    nguardian[guardcount].Master,nguardian[guardcount].City) == 4) {
+        guardcount++;
+        if (guardcount >= 35)
+        {
+            cout << "Max guardians reached." << endl;
+            break;
+        }            
+    }
+
     //Menu y selección sobre que hacer a continuación.
     do
     {
-        cout << "Welcome to the Rise of the guardians!\n Please select what you would like to do(Using values from 1 to 5):\n1)See the list of candidates.\n2)See the guardian.\n3)Meet the Kingdom.\n4)Watch a battle.\n5)Exit the game." << endl;
+        cout << "\nWelcome to the Rise of the guardians!\n Please select what you would like to do(Using values from 1 to 5):\n1)See the list of candidates.\n2)See the guardian.\n3)Meet the Kingdom.\n4)Watch a battle.\n5)Exit the game." << endl;
         cin >> select;
         switch (select)
         {
-            case 1: //Se imprimira en la consola los guardianes que son candidatos.
-                cout << "The list of candidates will be shown below:" << endl;
-
+            case 1: //Se imprimira en la consola los guardianes que son candidatos(Tienen que tener entre 90 y 99 puntos de poder).
+                cout << "The list of candidates will be shown below:";
+                for (int i = 0; i < guardcount; i++)
+                {
+                    if(nguardian[i].PowerLevel >= 90 && nguardian[i].PowerLevel <= 99)
+                    {
+                        printf("%s %d" ,nguardian[i].Name , nguardian[i].PowerLevel);
+                    }
+                }
+                cout << endl;
+                system("pause");
+                system("cls");
                 break;
             case 2:
-                cout << "See the guardian." << endl;
+                cout << "Showing all guardians: " << endl;
+                for (int i = 0; i < guardcount; i++)
+                {
+                    cout << i << ") " << nguardian[i].Name << "\n";
+                }
+
+                cout << "\nSelect the guardian that you want to see more info. (Using positive numbers)";
+                cin >> select;
+                while(select >= guardcount || select < 0)
+                {
+                    cout << "That's not a valid option, try again." << endl;
+                    cin >> select;
+                }                
+                cout << "Name: " << nguardian[select].Name << " PowerLevel: " <<nguardian[select].PowerLevel <<
+                " Master: " << nguardian[select].Master << " City: " << nguardian[select].City << endl;
+                system("pause");
                 break;
             case 3:
                 cout << "Meet the kingdom" << endl;
@@ -92,5 +131,4 @@ int main()
                 break;
         }
     } while (gameon == 1);
-    
 }
--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Curso: Algoritmos y estructura de datos Sección A.
 Profesor: Moscoso Dominguez Manuel Alejandro.
 Fecha de entrega: 27-11-2023.
 Creado con Visual Studio Code usando codigo C++.
-
