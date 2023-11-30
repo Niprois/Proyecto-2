8b17522c52d3c772c1e41bc68c761a7d85523358
Niprois <alvarof628@gmail.com>
2023-11-29 22:34:20 -0300

    Se añadio el sistema de batallas y el cambio de ranking despues de las mismas

---
+++ INSTALL.md
@@ -0,0 +1,8 @@
+Para poder iniciar y compilar este proyecto, necesita la version mas reciente de
+visual studio code, o su programa de preferencia, usando C++.
+Primero tiene que darle a File-> Open Folder -> Proyecto 2
+Segundo, tiene que seleccionar el archivo Proyecto2.cpp (Esto si quiere ver el codigo fuente.)
+Tercero, tiene que seleccionar Terminal -> New terminal.
+Cuarto, busca la carpeta donde tenga el proyecto usando la consola (Si es que al abrir no le aparece el directorio Proyecto2).
+Quinto, en la consola escribir g++ .\Proyecto2.cpp para compilar el proyecto.
+Sexto, escriba .\a.exe para ejecutar el proyecto.
--- Proyecto2.cpp
+++ Proyecto2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string.h>
+#include <ctime>
 #include <windows.h>
 
 using namespace std;
@@ -10,30 +11,70 @@ struct Guardian{   //Definición de guardian y sus caracteristicas, como nombre,
     int PowerLevel;
     char Master[20];
     char City[20];
-    string Apprentice[20];
+    int type; // 3=GM | 2=GR | 1=GC | 0=NONE
+    int ID;
+    string Apprentice[100];
 };
 struct Node { //Nodo para el arbol binario
     string ByName;    
     int data;
+    int dataT;
     Node* left;
     Node* right;
 
     // Constructor
-    Node(string strValue, int value) : ByName(strValue), data(value), left(nullptr), right(nullptr) {}
+    Node(string strValue, int value, int valuet) : ByName(strValue), data(value), dataT(valuet), left(nullptr), right(nullptr) {}
 };
+Node* findMin(Node* root) {
+    while (root->left != nullptr) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* deleteNode(Node* root, int value) {
+    if (root == nullptr) {
+        return root;
+    }
+
+    if (value < root->data) {
+        root->left = deleteNode(root->left, value);
+    } else if (value > root->data) {
+        root->right = deleteNode(root->right, value);
+    } else {
+        
+        if (root->left == nullptr) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == nullptr) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+
+    return root;
+}
+
 
 // Funcion para ingresar un nuevo nodo al arbol binario
-Node* insert(Node* root, string strValue,int value) {
+Node* insert(Node* root, string strValue,int value,int valueT) {
     // Si el arbol esta vacio, añade un nuevo nodo y lo convierte en raiz
     if (root == nullptr) {
-        return new Node(strValue, value);
+        return new Node(strValue, value, valueT);
     }
 
     // Si no es asi, recorre el arbol.
     if (value < root->data) {
-        root->left = insert(root->left, strValue,value);
+        root->left = insert(root->left, strValue,value,valueT);
     } else if (value > root->data) {
-        root->right = insert(root->right, strValue, value);
+        root->right = insert(root->right, strValue, value,valueT);
     }
 
     return root;
@@ -42,76 +83,24 @@ Node* insert(Node* root, string strValue,int value) {
 void inorderTraversal(Node* root) {
     if (root != nullptr) {
         inorderTraversal(root->left);
-        if (root->data >= 90 && root->data < 100) {
+        if (root->data >= 90 && root->data < 100 && root->dataT == 1) {
             cout <<  root->ByName  << " " << root->data;
         }
         inorderTraversal(root->right);
     }
 }
 
-template <typename T>
-class TreeNode {
-public:
-    T data;
-    vector<TreeNode*> children;
-
-    // Constructor
-    TreeNode(T value) : data(value) {}
-
-    // Destructor to free allocated memory
-    ~TreeNode() {
-        for (TreeNode* child : children) {
-            delete child;
-        }
-    }
-};
-
-// General Tree class to manage the general tree
-template <typename T>
-class GeneralTree {
-private:
-    TreeNode<T>* root;
-
-public:
-    // Constructor
-    GeneralTree(T rootData) {
-        root = new TreeNode<T>(rootData);
-    }
-
-    // Destructor to free allocated memory
-    ~GeneralTree() {
-        delete root;
-    }
-
-    // Function to add a child to a node
-    void addChild(TreeNode<T>* parent, T childData) {
-        TreeNode<T>* child = new TreeNode<T>(childData);
-        parent->children.push_back(child);
-    }
-
-    // Function to print the tree using depth-first traversal
-    void printTree(TreeNode<T>* node, int depth = 0) {
-        for (int i = 0; i < depth; ++i) {
-            std::cout << "  ";  // Indentation for better visualization
-        }
-        std::cout << node->data << std::endl;
-
-        for (TreeNode<T>* child : node->children) {
-            printTree(child, depth + 1);
-        }
-    }
-
-    // Public function to print the entire general tree
-    void printTree() {
-        printTree(root);
-    }
-};
-
 int main()
 {
     //Definición de parametros que se usaran en el futuro.
+    srand(static_cast<unsigned int>(time(nullptr)));
+    int valuetodelete;
     int gameon = 1;
+    int randomNumber;
+    int candidatecount = 0;
     int select;
+    int guard1 = 0;
+    int guard2 = 0;
     FILE *file = fopen("guardians.conf", "r");
     if(file == NULL) {
         perror("Error opening file.");
@@ -124,6 +113,15 @@ int main()
     //Se ingresan los guardianes dentro del struct
     while (fscanf(file, "%[^,],%d,%[^,],%[^\n]" , nguardian[guardcount].Name ,&nguardian[guardcount].PowerLevel,
     nguardian[guardcount].Master,nguardian[guardcount].City) == 4) {
+        nguardian[guardcount].ID = guardcount;
+        if(nguardian[guardcount].PowerLevel >= 90 && nguardian[guardcount].PowerLevel < 100)
+        {
+            nguardian[guardcount].type = 2;
+        }
+        if(nguardian[guardcount].PowerLevel == 100)
+        {
+            nguardian[guardcount].type = 3;
+        }
         guardcount++;
         if (guardcount >= 35)
         {
@@ -137,17 +135,16 @@ int main()
     Node* root = nullptr;
 
     // Ingresar PowerLevel y Nombre de los guardianes al arbol binario
-    root = insert(root, nguardian[0].Name, nguardian[0].PowerLevel);
+    root = insert(root, nguardian[0].Name, nguardian[0].PowerLevel ,nguardian[0].type);
     for (int i = 1; i < guardcount; i++)
     {
-        insert(root, nguardian[i].Name,nguardian[i].PowerLevel);
+        insert(root, nguardian[i].Name,nguardian[i].PowerLevel,nguardian[i].type);
     }
-
     
     //Menu y selección sobre que hacer a continuación.
     do
     {
-        cout << "\nWelcome to the Rise of the guardians!\n Please select what you would like to do(Using values from 1 to 5):\n1)See the list of candidates.\n2)See the guardian.\n3)Meet the Kingdom.\n4)Watch a battle.\n5)Exit the game." << endl;
+        cout << "\nWelcome to the Rise of the guardians!\n Please select what you would like to do(Using values from 1 to 5):\n1)See the list of candidates.\n2)See the guardian.\n3)Meet the Kingdom(Not implemented).\n4)Watch a battle.\n5)Exit the game." << endl;
         cin >> select;
         switch (select)
         {
@@ -155,32 +152,76 @@ int main()
                 cout << "The list of candidates will be shown below:";
                 inorderTraversal(root);
                 system("pause");
-                system("cls");
                 break;
-            case 2:
+            case 2://Permite ver la información detallada de los guardianes que usted desee.
                 cout << "Showing all guardians: " << endl;
                 for (int i = 0; i < guardcount; i++)
                 {
                     cout << i << ") " << nguardian[i].Name << "\n";
-                }
-
-                cout << "\nSelect the guardian that you want to see more info. (Using positive numbers)";
-                cin >> select;
-                while(select >= guardcount || select < 0)
-                {
-                    cout << "That's not a valid option, try again." << endl;
-                    cin >> select;
                 }                
+                cout << "\nSelect the guardian that you want to see more info. (Using positive numbers)";                
+                cin >> select;
                 cout << "Name: " << nguardian[select].Name << " PowerLevel: " <<nguardian[select].PowerLevel <<
-                " Master: " << nguardian[select].Master << " City: " << nguardian[select].City << endl;
+                " Master: " << nguardian[select].Master << " City: " << nguardian[select].City  <<  endl;
                 system("pause");
                 break;
             case 3:
-                cout << "Meet the kingdom" << endl;
+                cout << "Meet the kingdom (Not implemented.)" << endl;
                 break;
             case 4:
                 cout << "Watch a battle." << endl;
+                cout << "Select your guardian: ";
+                for (int i = 0; i < guardcount; i++)
+                {
+                    if (nguardian[i].type == 0)
+                    {
+                        cout << i << ") " << nguardian[i].Name << "\n";
+                    }
+                }
+                cin >> guard1;
+
+                cout << "Select your opponent: ";
+                for (int i = 0; i < guardcount; i++)
+                {
+                    cout << i << ") " << nguardian[i].Name << "\n";
+                }
+                cin >> guard2;
+                cout << "The guardian " << nguardian[guard1].Name << " will fight against " << nguardian[guard2].Name << endl;
+                randomNumber = rand() % 6 + 1;
+                cout << "The number generated is: " << randomNumber;
+                //Dependiendo de quien gane, se le suma o quita puntos.
+                if (randomNumber >= 5 && randomNumber <= 6)
+                { 
+                    cout << nguardian[guard1].Name << " wins the battle!" << endl; 
+                    valuetodelete = nguardian[guard1].PowerLevel;
+                    root = deleteNode(root, valuetodelete);
+                    nguardian[guard1].PowerLevel += 3;
+                    insert (root, nguardian[guard1].Name, nguardian[guard1].PowerLevel, nguardian[guard1].type);
+                    valuetodelete = nguardian[guard2].PowerLevel;
+                    root = deleteNode(root, valuetodelete);
+                    nguardian[guard2].PowerLevel -= 1;
+                    insert (root, nguardian[guard2].Name, nguardian[guard2].PowerLevel, nguardian[guard2].type);                    
+                }
+                else
+                {
+                    cout << nguardian[guard2].Name << " wins the battle!" << endl;
+                    valuetodelete = nguardian[guard2].PowerLevel;
+                    root = deleteNode(root, valuetodelete);
+                    nguardian[guard2].PowerLevel += 3;
+                    if(nguardian[guard2].PowerLevel >= 90 && nguardian[guard2].PowerLevel <= 99)
+                    {
+                        nguardian[guard2].type = 1;
+                    }
+                    insert (root, nguardian[guard2].Name, nguardian[guard2].PowerLevel, nguardian[guard2].type);
+                    valuetodelete = nguardian[guard1].PowerLevel;
+                    root = deleteNode(root, valuetodelete);
+                    nguardian[guard1].PowerLevel -= 1;
+                    insert (root, nguardian[guard1].Name, nguardian[guard1].PowerLevel, nguardian[guard1].type);  
+                }
+                cout << "Updating the ranking..." << endl;
+                system("pause");
                 break;
+            //Salir del juego
             case 5:
                 cout << "Exit the game." << endl;
                 return 0;                
